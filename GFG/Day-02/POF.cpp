d043de2d64a2e4282302cfe6fe10b6bc7c773765
suryakant-source <xbsuryakant@gmail.com>
2025-10-29 06:46:12 +0530

    Create POF.cpp

---
+++ GFG/Day-02/POF.cpp
@@ -0,0 +1,54 @@
+/*You are given an undirected graph with V vertices numbered from 0 to V-1 and E edges, represented as a 2D array edges[][], where each element edges[i] = [u, v] represents an undirected edge between vertex u and vertex v. Find the diameter of the graph.
+The diameter of a graph (sometimes called the width) is the number of edges on the longest path between two vertices in the graph.
+
+Examples :
+
+Input: V = 6, E = 5, edges[][] = [[0, 1], [0, 4], [1, 3], [1, 2], [2, 5]]
+    
+Output: 4
+Explanation: The longest path in the graph is from vertices 4 to vertices 5 (4 -> 0 -> 1 -> 2 -> 5).
+Input: V = 7, E = 6, edges[][] = [[0, 2], [0, 4], [0, 3], [3, 1], [3, 5], [1, 6]]
+    
+Output: 4
+Explanation: The longest path in the graph is from vertices 2 to vertices 6 (2 -> 0 -> 3 -> 1 -> 6).
+Constraints:
+2 ≤ V ≤  105
+1 ≤ E ≤  V - 1
+0 ≤ edges[i][0], edges[i][1] < V*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int diameter(int V, vector<vector<int>>& edges) {
+        
+        vector<vector<int>> adj(V);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        auto bfs = [&](int start) -> pair<int, int> {
+            vector<int> dist(V, -1);
+            queue<int> q;
+            q.push(start);
+            dist[start] = 0;
+
+            int farthestNode = start;
+            while (!q.empty()) {
+                int node = q.front(); q.pop();
+                for (int nei : adj[node]) {
+                    if (dist[nei] == -1) {
+                        dist[nei] = dist[node] + 1;
+                        q.push(nei);
+                        if (dist[nei] > dist[farthestNode])
+                            farthestNode = nei;
+                    }
+                }
+            }
+            return {farthestNode, dist[farthestNode]};
+        };
+        auto [nodeA, _] = bfs(0);
+        auto [nodeB, diameter] = bfs(nodeA);
+        return diameter;
+    }
+};
