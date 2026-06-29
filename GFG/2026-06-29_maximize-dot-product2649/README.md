bfbfb028326cbc3518a1231d9b850cfba4b92d2e
suryakant-source <xbsuryakant@gmail.com>
2026-06-29 06:59:00 +0530

    Auto-solved daily problems on 2026-06-29

---
+++ GFG/2026-06-29_maximize-dot-product2649/README.md
@@ -0,0 +1,8 @@
+# Max Dot Product with 0 Insertions
+
+**Platform:** GFG
+**URL:** [https://www.geeksforgeeks.org/problems/maximize-dot-product2649/1](https://www.geeksforgeeks.org/problems/maximize-dot-product2649/1)
+
+## Description
+
+Back to Explore Page
---
+++ GFG/2026-06-29_maximize-dot-product2649/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDotProduct(self, a: list[int], b: list[int]) -> int:
+        m, n = len(a), len(b)
+        memo = {}
+
+        def dp(i, j):
+            if i == m or j == n:
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            ans = max(a[i]*b[j] + dp(i+1, j+1), dp(i+1, j), dp(i, j+1))
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
