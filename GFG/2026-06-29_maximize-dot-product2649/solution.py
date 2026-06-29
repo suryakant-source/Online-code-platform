226235736a9e8dcdd61aea9366d108ec699faabc
suryakant-source <xbsuryakant@gmail.com>
2026-06-29 07:18:29 +0530

    Auto-solved daily problems on 2026-06-29

--- GFG/2026-06-29_maximize-dot-product2649/solution.py
+++ GFG/2026-06-29_maximize-dot-product2649/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def maxDotProduct(self, a, b):
-        m, n = len(a), len(b)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                dp[i][j] = max(dp[i-1][j-1] + a[i-1]*b[j-1], dp[i-1][j], dp[i][j-1])
-        
-        return dp[m][n]
+        n, m = len(a), len(b)
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i == n or j == m:
+                return 0
+            ans = max(dp(i + 1, j), a[i] * b[j] + dp(i + 1, j + 1), dp(i, j + 1))
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
