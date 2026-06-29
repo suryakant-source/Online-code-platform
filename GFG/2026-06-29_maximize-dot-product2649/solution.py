6b7682ff9a4162cd52a4c112fa49f26d9d7c096e
suryakant-source <xbsuryakant@gmail.com>
2026-06-29 07:12:23 +0530

    Auto-solved daily problems on 2026-06-29

--- GFG/2026-06-29_maximize-dot-product2649/solution.py
+++ GFG/2026-06-29_maximize-dot-product2649/solution.py
@@ -5,6 +5,6 @@ class Solution:
         
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                dp[i][j] = max(dp[i - 1][j - 1] + a[i - 1] * b[j - 1], dp[i - 1][j], dp[i][j - 1])
+                dp[i][j] = max(dp[i-1][j-1] + a[i-1]*b[j-1], dp[i-1][j], dp[i][j-1])
         
         return dp[m][n]
